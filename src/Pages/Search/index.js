c3251cc64ba6f675baed6bee22906138280ab2f8
12yu43 <awasthideeksha123@gmail.com>
2024-09-15 20:25:55 +0530

    build correct

--- src/Pages/Search/index.js
+++ src/Pages/Search/index.js
@@ -1,275 +1,129 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react';
 import { Link, useLocation, useParams } from 'react-router-dom';
 import { FetchApi } from '../../API/FetchApi';
 import { Endpoints } from '../../API/Endpoints';
 import Subscribe from '../Home/Subscribe';
 import Follow from '../Home/Follow';
-import { Helmet } from "react-helmet";
+import { Helmet } from 'react-helmet';
 import Header from '../../Layout/Header';
 import Footer from '../../Layout/Footer';
 import Loader from '../Loader/Index';
 
-export default function Search() {
-  const [TechnologyData, setTechnologyData] = useState([]);
-  const [IndustryData, setIndustryData] = useState([]);
-  const [MagazineData, setMagazineData] = useState([]);
-  const [CxoData, setCxoData] = useState([]);
-  const [StartupData, setStartupData] = useState([]);
-  const [loader, setLoader] = useState(false);
-  const param = useParams();
-  useEffect(() => {
-    handleGetData();
-  }, [param?.search]);
-
+const Search = () => {
+  const [technologyData, setTechnologyData] = useState([]);
+  const [industryData, setIndustryData] = useState([]);
+  const [magazineData, setMagazineData] = useState([]);
+  const [cxoData, setCxoData] = useState([]);
+  const [startupData, setStartupData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { search } = useParams();
   const location = useLocation();
 
+  // Scroll to top on route change
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-  }, [param?.slug, location.pathname]);
+  }, [search, location.pathname]);
 
-  const handleGetData = async () => {
-    setLoader(true);
+  // Fetch data
+  const handleGetData = useCallback(async () => {
+    setLoading(true);
     try {
-      let resp = await FetchApi(Endpoints.GetSearch + "/" + param?.search);
-      if (resp && resp.status === true) {
-        setTechnologyData(resp.data?.technology);
-        setIndustryData(resp.data?.industry);
-        setMagazineData(resp.data?.magazine);
-        setCxoData(resp.data?.cxo);
-        setStartupData(resp.data?.startup);
-        setLoader(false);
+      const resp = await FetchApi(`${Endpoints.GetSearch}/${search}`);
+      if (resp && resp.status) {
+        setTechnologyData(resp.data?.technology || []);
+        setIndustryData(resp.data?.industry || []);
+        setMagazineData(resp.data?.magazine || []);
+        setCxoData(resp.data?.cxo || []);
+        setStartupData(resp.data?.startup || []);
       }
+    } catch (e) {
+      console.error('Error fetching data:', e);
+    } finally {
+      setLoading(false);
     }
-    catch (e) {
-      if (e && e.response && e.response.data && e.response.data.message) {
-        console.log(e.response.data.message)
-      }
-    }
-  };
+  }, [search]);
+
+  useEffect(() => {
+    handleGetData();
+  }, [handleGetData]);
+
+  const renderPosts = (data, category) => (
+    data.map((item, index) => (
+      <div className="post fashion-post post-default-list post-separator-border" key={index}>
+        <div className="post-wrap">
+          <Link className="image" to={`/${category}/${item?.url}`}>
+            <img src={`${Endpoints.ImageUrl}${item?.images}`} alt={item?.image_alt} className="client-image" />
+          </Link>
+          <div className="content">
+            <h4 className="title">
+              <Link to={`/${category}/${item?.url}`}>
+                {item?.title?.length > 50 ? `${item?.title.substring(0, 50)}...` : item?.title}
+              </Link>
+            </h4>
+            <p>
+              {item?.meta_description?.length > 100 ? `${item?.meta_description.substring(0, 100)}...` : item?.meta_description}
+            </p>
+          </div>
+        </div>
+      </div>
+    ))
+  );
 
   return (
     <>
-      {loader ?
+      {loading ? (
         <Loader />
-        :
+      ) : (
         <>
           <Header />
           <Helmet>
             <title>The Executive Headlines - Top business magazine & news headlines sources</title>
           </Helmet>
-          <div class="page-banner-section section mt-30 mb-30">
-            <div class="container">
-              <div class="row row-1">
-
-                <div class="col-lg-12 col-12">
-                  <div class="page-banner" style={{ "background-image": "url(../img/bg/page-banner-fashion.jpg)" }}>
-                    <h2><span class="category-fashion">Search</span></h2>
-                    <ol class="page-breadcrumb">
-                      <li><a href="/">Home</a></li>
-                      <li class="active">Search</li>
+          <div className="page-banner-section section mt-30 mb-30">
+            <div className="container">
+              <div className="row row-1">
+                <div className="col-lg-12 col-12">
+                  <div className="page-banner" style={{ backgroundImage: 'url(../img/bg/page-banner-fashion.jpg)' }}>
+                    <h2><span className="category-fashion">Search</span></h2>
+                    <ol className="page-breadcrumb">
+                      <li><Link to="/">Home</Link></li>
+                      <li className="active">Search</li>
                     </ol>
-
                   </div>
                 </div>
-
-
               </div>
             </div>
           </div>
 
-          <div class="post-section section mt-50">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-8 col-12 mb-50">
-                  <div class="post-block-wrapper">
-                    <div class="body">
-                      {TechnologyData?.map((item, index) => {
-                        return (
-                          <div class="post fashion-post post-default-list post-separator-border" key={index}>
-                            <div class="post-wrap">
-
-
-                              <Link class="image" to={item?.cat_slug + "/" + item?.url}><img src={Endpoints.ImageUrl + item?.images} alt={item?.image_alt} className="client-image" /></Link>
-
-                              <div class="content">
-
-
-                                <h4 class="title"><Link to={item?.cat_slug + "/" + item?.url}>{item?.title?.length > 50 ? item?.title?.substring(0, 50) + "..." : item?.title}</Link></h4>
-
-                                <div class="meta fix">
-
-                                  {/* <span class="meta-item date"><i class="fa fa-clock-o"></i>{moment(item?.created_at).format("DD MMM YYYY")}</span> */}
-                                </div>
-
-
-                                <p>{item?.meta_description?.length > 100 ? item?.meta_description?.substring(0, 100) + "..." : item?.meta_description}</p>
-
-
-                                {/* <Link to="/post-detail" class="read-more">continue reading</Link> */}
-
-                              </div>
-
-                            </div>
-                          </div>
-                        )
-                      })}
-
-                      {IndustryData?.map((item, index) => {
-                        return (
-                          <div class="post fashion-post post-default-list post-separator-border" key={index}>
-                            <div class="post-wrap">
-
-
-                              <Link class="image" to={item?.cat_slug + "/" + item?.url}><img src={Endpoints.ImageUrl + item?.images} alt={item?.image_alt} className="client-image" /></Link>
-
-                              <div class="content">
-
-
-                                <h4 class="title"><Link to={item?.cat_slug + "/" + item?.url}>{item?.title?.length > 50 ? item?.title?.substring(0, 50) + "..." : item?.title}</Link></h4>
-
-                                <div class="meta fix">
-
-                                  {/* <span class="meta-item date"><i class="fa fa-clock-o"></i>{moment(item?.created_at).format("DD MMM YYYY")}</span> */}
-                                </div>
-
-
-                                <p>{item?.meta_description?.length > 100 ? item.meta_description.substring(0, 100) + "..." : item.meta_description}</p>
-
-
-                                {/* <Link to="/post-detail" class="read-more">continue reading</Link> */}
-
-                              </div>
-
-                            </div>
-                          </div>
-                        )
-                      })}
-
-
-                      {MagazineData?.map((item, index) => {
-                        return (
-                          <div class="post fashion-post post-default-list post-separator-border" key={index}>
-                            <div class="post-wrap">
-
-
-                              <Link class="image" to={"/magazine/" + item?.url}><img src={Endpoints.ImageUrl + item?.issue_logo} alt={item?.image_alt} className="client-image" /></Link>
-
-                              <div class="content">
-
-
-                                <h4 class="title"><Link to={"/magazine/" + item?.url}>{item?.issue_title?.length > 50 ? item?.issue_title?.substring(0, 50) + "..." : item?.issue_title}</Link></h4>
-
-                                <div class="meta fix">
-
-                                  {/* <span class="meta-item date"><i class="fa fa-clock-o"></i>{moment(item?.created_at).format("DD MMM YYYY")}</span> */}
-                                </div>
-
-
-                                <p>{item.meta_description.length > 100 ? item.meta_description.substring(0, 100) + "..." : item.meta_description}</p>
-
-
-                                {/* <Link to="/post-detail" class="read-more">continue reading</Link> */}
-
-                              </div>
-
-                            </div>
-                          </div>
-                        )
-                      })}
-
-
-                      {CxoData?.map((item, index) => {
-                        return (
-                          <div class="post fashion-post post-default-list post-separator-border" key={index}>
-                            <div class="post-wrap">
-
-
-                              <Link class="image" to={"/cxo/" + item?.url}><img src={Endpoints.ImageUrl + item?.images} alt={item?.image_alt} className="client-image" /></Link>
-
-                              <div class="content">
-
-
-                                <h4 class="title"><Link to={"/cxo/" + item?.url}>{item?.title?.length > 50 ? item?.title?.substring(0, 50) + "..." : item?.title}</Link></h4>
-
-                                <div class="meta fix">
-
-                                  {/* <span class="meta-item date"><i class="fa fa-clock-o"></i>{moment(item?.created_at).format("DD MMM YYYY")}</span> */}
-                                </div>
-
-
-                                <p>{item?.meta_description?.length > 100 ? item?.meta_description?.substring(0, 100) + "..." : item?.meta_description}</p>
-
-
-                                {/* <Link to="/post-detail" class="read-more">continue reading</Link> */}
-
-                              </div>
-
-                            </div>
-                          </div>
-                        )
-                      })}
-
-                      {StartupData?.map((item, index) => {
-                        return (
-                          <div class="post fashion-post post-default-list post-separator-border" key={index}>
-                            <div class="post-wrap">
-
-
-                              <Link class="image" to={"/startup-insights/" + item?.url}><img src={Endpoints.ImageUrl + item?.images} alt={item?.image_alt} className="client-image" /></Link>
-
-                              <div class="content">
-
-
-                                <h4 class="title"><Link to={"/startup-insights/" + item?.url}>{item?.title?.length > 50 ? item?.title?.substring(0, 50) + "..." : item?.title}</Link></h4>
-
-                                <div class="meta fix">
-
-                                  {/* <span class="meta-item date"><i class="fa fa-clock-o"></i>{moment(item?.created_at).format("DD MMM YYYY")}</span> */}
-                                </div>
-
-
-                                <p>{item?.meta_description?.length > 100 ? item?.meta_description?.substring(0, 100) + "..." : item?.meta_description}</p>
-
-
-                                {/* <Link to="/post-detail" class="read-more">continue reading</Link> */}
-
-                              </div>
-
-                            </div>
-                          </div>
-                        )
-                      })}
-
-
-
-
-
-
-
+          <div className="post-section section mt-50">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-8 col-12 mb-50">
+                  <div className="post-block-wrapper">
+                    <div className="body">
+                      {renderPosts(technologyData, 'technology')}
+                      {renderPosts(industryData, 'industry')}
+                      {renderPosts(magazineData, 'magazine')}
+                      {renderPosts(cxoData, 'cxo')}
+                      {renderPosts(startupData, 'startup-insights')}
                     </div>
                   </div>
-
                 </div>
 
-
-                <div class="col-lg-4 col-12 mb-50">
-                  <div class="row">
+                <div className="col-lg-4 col-12 mb-50">
+                  <div className="row">
                     <Follow />
-
-
-
                     <Subscribe />
-
                   </div>
                 </div>
-
               </div>
-
             </div>
           </div>
           <Footer />
         </>
-      }
+      )}
     </>
-  )
-}
+  );
+};
+
+export default Search;
--- src/Pages/Technology/Deatil.js
+++ src/Pages/Technology/Deatil.js
@@ -1,189 +1,158 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Link, useLocation, useParams } from 'react-router-dom';
 import { FetchApi } from '../../API/FetchApi';
 import { Endpoints } from '../../API/Endpoints';
-
-
-import { Helmet } from "react-helmet";
+import { Helmet } from 'react-helmet';
 import Header from '../../Layout/Header';
 import Footer from '../../Layout/Footer';
 import Loader from '../Loader/Index';
-export default function Deatil() {
-    const [data, setData] = useState("");
-    const [MagazineData, setMagazineData] = useState("");
-    const [loader, setLoader] = useState(false);
-    const param = useParams();
+
+const Deatil = () => {
+    const [data, setData] = useState(null);
+    const [magazineData, setMagazineData] = useState(null);
+    const [loader, setLoader] = useState(true);
+    const { slug } = useParams();
     const location = useLocation();
 
-    useEffect(() => {
-        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-        getData();
-    }, [param?.slug, location.pathname]);
-
-    const getData = async () => {
+    const getData = useCallback(async () => {
         setLoader(true);
         let type = "technology";
-        if (location.pathname.split("/")[1] === "technology") {
-            type = "technology";
+
+        const path = location.pathname.split("/")[1].toLowerCase();
+        switch (path) {
+            case "technology":
+                type = "technology";
+                break;
+            case "healthcare":
+            case "retail":
+            case "telecom":
+            case "banking-finance":
+            case "education":
+            case "legal":
+            case "media-entertainment":
+            case "erp":
+            case "digital-marketing":
+            case "business":
+            case "food-beverage":
+                type = "industry";
+                break;
+            case "cxo":
+                type = "cxo";
+                break;
+            case "startup-insights":
+                type = "startup-insight";
+                break;
+            case "feature":
+                type = "featured-people";
+                break;
+            case "cover-story":
+                type = "cover-story";
+                break;
+            case "news-detail":
+            case "sports":
+            case "lifestyle":
+            case "entrepreneurs":
+            case "entertainment-media":
+            case "awards-events":
+                type = "new-category";
+                break;
+            default:
+                type = "technology";
         }
-        if (location.pathname.split("/")[1] === "Healthcare"
-            || location.pathname.split("/")[1] === "healthcare"
-            || location.pathname.split("/")[1] === "Retail"
-            || location.pathname.split("/")[1] === "retail"
-            || location.pathname.split("/")[1] === "Telecom"
-            || location.pathname.split("/")[1] === "telecom"
-            || location.pathname.split("/")[1] === "banking-finance"
-            || location.pathname.split("/")[1] === "education"
-            || location.pathname.split("/")[1] === "legal"
-            || location.pathname.split("/")[1] === "media-entertainment"
-            || location.pathname.split("/")[1] === "ERP"
-            || location.pathname.split("/")[1] === "erp"
-            || location.pathname.split("/")[1] === "digital-marketing"
-            || location.pathname.split("/")[1] === "Business"
-            || location.pathname.split("/")[1] === "business"
-            || location.pathname.split("/")[1] === "food-beverage"
-        ) {
-            type = "industry";
-        }
-        // if (location.pathname.split("/")[1] === "business-news-detail") {
-        //     type = "bussiness-news";
-        // }
-        if (location.pathname.split("/")[1] === "cxo") {
-            type = "cxo";
-        }
-        if (location.pathname.split("/")[1] === "startup-insights") {
-            type = "startup-insight";
-        }
-        if (location.pathname.split("/")[1] === "feature") {
-            type = "featured-people";
-        }
-        if (location.pathname.split("/")[1] === "cover-story") {
-            type = "cover-story";
-        }
-        if (location.pathname.split("/")[1] === "news-detail"
-            || location.pathname.split("/")[1] === "sports"
-            || location.pathname.split("/")[1] === "lifestyle"
-            || location.pathname.split("/")[1] === "entrepreneurs"
-            || location.pathname.split("/")[1] === "entertainment-media"
-            || location.pathname.split("/")[1] === "awards-events"
-        ) {
-            type = "new-category";
-        }
-        // if (location.pathname.split("/")[1] === "popular-news-detail") {
-        //     type = "top-category";
-        // }
+
         try {
-            let resp = await FetchApi(Endpoints.GetNewsDetail + "/" + type + "/" + param?.slug);
-            if (resp && resp.status === true) {
-                if (location.pathname.split("/")[1] === "feature") {
+            const resp = await FetchApi(`${Endpoints.GetNewsDetail}/${type}/${slug}`);
+            if (resp && resp.status) {
+                if (path === "feature") {
                     setData(resp.data?.people);
                     setMagazineData(resp.data?.magazine);
-                } else if (location.pathname.split("/")[1] === "cover-story") {
+                } else if (path === "cover-story") {
                     setData(resp.data?.CoverStory);
                     setMagazineData(resp.data?.magazine);
                 } else {
                     setData(resp.data);
                 }
                 setLoader(false);
+            }
+        } catch (e) {
+            console.error('Error fetching data:', e.response?.data?.message || e.message);
+        }
+    }, [location.pathname, slug]);
 
-            }
-        }
-        catch (e) {
-            if (e && e.response && e.response.data && e.response.data.message) {
-                console.log(e.response.data.message)
-            }
-        }
+    useEffect(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+        getData();
+    }, [getData]);
+
+    if (loader) {
+        return <Loader />;
     }
+
+    const path = location.pathname.split("/")[1];
+
     return (
         <>
-            {loader ?
-                <Loader />
-                :
-                <>
-                    <Header />
-                    <Helmet>
-                        <title>{data?.meta_title}</title>
-                    </Helmet>
-                    <div className="post-header-section section mt-30 mb-30">
-                        <div className="container">
-                            <div className="row row-1">
-                                <div className="col-12">
-                                    <div className="post-header" style={{ "background-image": "url(../img/bg/page-banner-fashion.jpg)" }}>
-                                        <h3 className="title">{data?.title}</h3>
-                                        <div className="meta fix">
-                                            <a href="#" className="meta-item category fashion">{data?.cat_slug}</a>
-                                            {/* <a href="#" className="meta-item author"><img src="../img/post/post-author-1.jpg" alt="post author" />Sathi Bhuiyan</a> */}
-                                            {/* <span className="meta-item date"><i className="fa fa-clock-o"></i>{moment(data?.created_at).format("DD MMM YYYY")}</span> */}
-
-                                        </div>
-
-                                    </div>
+            <Header />
+            <Helmet>
+                <title>{data?.meta_title || 'Detail Page'}</title>
+            </Helmet>
+            <div className="post-header-section section mt-30 mb-30">
+                <div className="container">
+                    <div className="row row-1">
+                        <div className="col-12">
+                            <div className="post-header" style={{ backgroundImage: 'url(../img/bg/page-banner-fashion.jpg)' }}>
+                                <h3 className="title">{data?.title}</h3>
+                                <div className="meta fix">
+                                    <span className="meta-item category fashion">{data?.cat_slug}</span>
                                 </div>
-
                             </div>
                         </div>
                     </div>
-                    <div className="post-section section">
-                        <div className="container">
-                            <div className="row">
-
-                                <div className="col-lg-12 col-12 mb-50">
-                                    {location.pathname.split("/")[1] === "feature" ?
-                                        <div className="mb-20 text-center w-100 ">
-                                            <Link to={"/magazine/" + MagazineData?.url}>
-                                                <img src={Endpoints.ImageUrl + MagazineData?.issue_logo} alt="" className="w-50 border p-2" />
-                                            </Link>
-
-                                        </div>
-                                        : ""}
-
-                                    {location.pathname.split("/")[1] === "cover-story" ?
-                                        <div className="mb-20 text-center w-100 ">
-                                            <Link to={"/magazine/" + data?.pre_link}>
-                                                <img src={Endpoints.ImageUrl + MagazineData?.issue_logo} alt="" className="w-50 border p-2" />
-                                            </Link>
-
-                                        </div>
-                                        : ""}
-
-                                    <div className="text-center w-100 mb-20">
-                                        <img src={Endpoints.ImageUrl + data?.images} alt="" className="w-50" />
-                                    </div>
-
-
-                                    <div className="post-block-wrapper mb-50 mt-0">
-                                        <div className="body">
-                                            <div className="row">
-                                                <div className="col-12">
-                                                    <div className="single-post">
-                                                        <div className="post-wrap">
-                                                            <div className="">
-                                                                <p className="" dangerouslySetInnerHTML={{ __html: data?.content_details }}></p>
-
-                                                            </div>
-
-
-
-                                                        </div>
+                </div>
+            </div>
+            <div className="post-section section">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12 col-12 mb-50">
+                            {path === "feature" && magazineData && (
+                                <div className="mb-20 text-center w-100">
+                                    <Link to={`/magazine/${magazineData?.url}`}>
+                                        <img src={`${Endpoints.ImageUrl}${magazineData?.issue_logo}`} alt={magazineData?.url} className="w-50 border p-2" />
+                                    </Link>
+                                </div>
+                            )}
+                            {path === "cover-story" && magazineData && (
+                                <div className="mb-20 text-center w-100">
+                                    <Link to={`/magazine/${data?.pre_link}`}>
+                                        <img src={`${Endpoints.ImageUrl}${magazineData?.issue_logo}`} alt={data?.pre_link} className="w-50 border p-2" />
+                                    </Link>
+                                </div>
+                            )}
+                            <div className="text-center w-100 mb-20">
+                                <img src={`${Endpoints.ImageUrl}${data?.images}`} alt={data?.title} className="w-50" />
+                            </div>
+                            <div className="post-block-wrapper mb-50 mt-0">
+                                <div className="body">
+                                    <div className="row">
+                                        <div className="col-12">
+                                            <div className="single-post">
+                                                <div className="post-wrap">
+                                                    <div className="">
+                                                        <p className="" dangerouslySetInnerHTML={{ __html: data?.content_details || '' }}></p>
                                                     </div>
-
                                                 </div>
-
                                             </div>
                                         </div>
-
                                     </div>
-
                                 </div>
-
-
                             </div>
                         </div>
                     </div>
-                    <Footer />
-                </>
-            }
+                </div>
+            </div>
+            <Footer />
         </>
-    )
-}
+    );
+};
 
+export default Deatil;
